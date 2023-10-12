609f8c1b50e6fac9a715ff12de5bb1a156e26419
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-12 02:17:57 -0400

    First lines of code in Java!

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "This is my first java code!";
+        System.out.println(message.endsWith("!"));
+    }
+}
