b71cb621f56bacb30b19222abf6aa6f76bead0d9
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-25 11:18:54 -0400

    Some more practice with Java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
+        sayHello("Jordan", 23);
+        fizzBuzz();
+    }
+
+    public static void sayHello(String name, int age) {
+        System.out.println("Hello " + name + "!" + " It seems your age is " + age);
+    }
+
+    public static void fizzBuzz() {
         // FizzBuzz Exercise
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is your number?: ");
